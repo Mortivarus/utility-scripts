dd861f8d99c2fbb2d3b9211f32cb5d709895ab59
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2025-07-01 12:42:14 +0200

    Create libvirt-setup.sh

---
+++ setup/libvirt-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#install through apt
+
+# Require sudo
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run with sudo."
+  exit 1
+fi
+
+apt install virt-manager libvirt-daemon-system-sysv  gir1.2-spiceclientglib-2.0  gir1.2-spiceclientgtk-3.0  qemu-utils  virt-viewer  qemu-system-x86 libvirt-daemon-system
+
+usermod -a -G libvirt "$SUDO_USER"
