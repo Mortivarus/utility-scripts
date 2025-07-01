7dc35c2a57cf25907f1e01b3f536d5b6d2e3d3ce
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2025-07-01 23:37:38 +0200

    Update gog-game-template.sh

--- setup/gog-game-template.sh
+++ setup/gog-game-template.sh
@@ -4,11 +4,11 @@
 prefixdir="$HOME/games"
 identifier="my-game"
 winebin="wine"
-setupfile="$HOME/Downloads/setup.exe"
+setupfile="setup.exe"
 prefix="$prefixdir/$identifier/pfx"
 
 
 mkdir -p $prefix
 
 # Install the game in 'prefixdir/identifier/pfx/drive_c/game' and show no notifications or create desktop or start menu icons
-WINEPREFIX="$prefix" $winebin "$setupfile" -c /DIR='c:\\game' /NOICONS /VERYSILENT
+WINEPREFIX="$prefix" "$winebin" "$setupfile" -c /DIR='c:\\game' /NOICONS /VERYSILENT
