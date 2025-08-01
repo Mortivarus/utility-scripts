7069a8d15e7ceaefd3307f927429d3d050154362
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2025-08-01 22:18:53 +0200

    Update conda-setup.sh

--- setup/conda-setup.sh
+++ setup/conda-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/conda
+
 $HOME/conda/bin/conda init
+
+conda config --add channels conda-forge
 rm Miniconda3-latest-Linux-x86_64.sh 
